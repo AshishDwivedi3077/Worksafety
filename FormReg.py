4e9e8502edb73d9d4f2180fdc8500e0bc9d8911f
Ashishkumar Dwivedi <ashishkumd2@birlasoft.com>
2020-07-28 10:36:11 +0530

    new project added

---
+++ FormReg.py
@@ -0,0 +1,137 @@
+from flask import Flask, render_template,request,redirect
+import json
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import os, uuid
+
+app = Flask(__name__)
+
+def listtodict(A, dic):
+   dic = dict(A)
+   return dic
+# Home================================================================================================================================
+@app.route("/")
+def home():
+    return render_template("Index.html")
+# Load Grid ===========================================================================================================================
+JsonResult=None
+
+ #blob_service_client = BlobServiceClient.from_connection_string('DefaultEndpointsProtocol=https;AccountName=checklistform;AccountKey=dpZeiTIELCgVGxRxqYkhqAx42b8pWND2onchJ83+yXgHqbkMkPS5aKbmmVMtdDJdENFmmH3EuHhANmUQ02d7TQ==;EndpointSuffix=core.windows.net')
+
+@app.route("/LoadGridFromBlob",methods=['GET','POST'])
+def LoadGridFromBlob():
+    resp = request.form
+    global blob_service_client
+    blob_service_client = BlobServiceClient.from_connection_string(resp['ConnectionStr'])
+
+    container_client = blob_service_client.get_container_client(resp["Container"])
+
+    analysed_Json = []
+    blob_list = container_client.list_blobs()
+    for blob in blob_list:
+        analysed_Json.append(blob.name)
+    global fileName
+    if  resp["blob"] =="":
+        fileName = analysed_Json[0]
+    else:
+        fileName =  resp['blob']
+
+
+    download_stream = container_client.download_blob(fileName)
+    jsonData = download_stream.readall()
+    result = json.loads(jsonData)
+    global JsonResult
+    JsonResult=result
+    fields = result['analyzeResult']['documentResults'][0]['fields']
+
+    q = []
+    a = []
+    q.append("FileName");
+    # a.append(analysed_Json[int(resp['page'])])
+    a.append(analysed_Json)
+
+    Name = result['analyzeResult']['documentResults'][0]['fields']['Name']
+    Name = get_key(fields,Name)
+    NameValue = result['analyzeResult']['documentResults'][0]['fields']['Name']['text']
+    q.append(Name)
+    a.append(NameValue)
+
+    Name = result['analyzeResult']['documentResults'][0]['fields']['Employee Code']
+    Name = get_key(fields,Name)
+    NameValue = result['analyzeResult']['documentResults'][0]['fields']['Employee Code']['text']
+    q.append(Name)
+    a.append(NameValue)
+
+    Name = result['analyzeResult']['documentResults'][0]['fields']['Company']
+    Name = get_key(fields,Name)
+    NameValue = result['analyzeResult']['documentResults'][0]['fields']['Company']['text']
+    q.append(Name)
+    a.append(NameValue)
+
+    for i in range(1, 14, 1):
+        Name = result['analyzeResult']['documentResults'][0]['fields']['question' + str(i)]
+        Name = get_key(fields,Name)
+        NameValue = result['analyzeResult']['documentResults'][0]['fields']['question' + str(i)]['text']
+        q.append(Name)
+        a.append(NameValue)
+
+
+
+    for i in range(1, 19, 1):
+        Name = result['analyzeResult']['documentResults'][0]['fields']['answer' + str(i)]
+        for i in range(1, 19, 1):
+            Name = result['analyzeResult']['documentResults'][0]['fields']['answer' + str(i)]
+            if Name != None:
+                Name = get_key(fields,Name)
+                NameValue = result['analyzeResult']['documentResults'][0]['fields']['answer' + str(i)]['text']
+                q.append(Name)
+                a.append(NameValue)
+            else:
+                result['analyzeResult']['documentResults'][0]['fields']['answer' + str(i)] = {'type': 'string',
+                                                                                              'valueString': '✔',
+                                                                                              'text': '✔', 'page': 1,
+                                                                                              'boundingBox': [],
+                                                                                              'confidence': 0,
+                                                                                              'elements': []}
+
+                Name = get_key(fields,Name)
+                NameValue = result['analyzeResult']['documentResults'][0]['fields']['answer' + str(i)]['text']
+                q.append(Name)
+                a.append(NameValue)
+
+    # a_file = open("templates/JsonData/Result-WorkspaceInspection"+ str(resp['page']) +".json", "w")
+    # json.dump(result, a_file)
+    # a_file.close()
+
+    finalData = dict(zip(q, a))
+    return finalData
+
+
+@app.route("/UpdateJsonToBlob",methods=['POST'])
+def UpdateJsonToBlob():
+    resp = request.form
+    global JsonResult
+    result = JsonResult
+    print(result)
+    dataList={}
+    resp=listtodict(resp,dataList)
+    print(resp['answer'])
+    result['analyzeResult']['documentResults'][0]['fields'][resp['Labels']]['text'] = resp['answer']
+    result['analyzeResult']['documentResults'][0]['fields'][resp['Labels']]['valueString'] = resp['answer']
+    EditedResult=result
+    blob_client = blob_service_client.get_container_client("editedjson")
+    blob_client.upload_blob(fileName, json.dumps(result), overwrite=True)
+
+    print(resp)
+    print(resp['Labels'])
+
+    return resp
+
+def get_key(fields,val):
+    for key, value in fields.items():
+        if val == value:
+            return key
+    return "key doesn't exist"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/JsFiles/FormJs.js
@@ -0,0 +1,206 @@
+var count=0;
+
+$(document).ready(function(){
+    $("#GetBlob" ).click(function() {
+        var CntStr=$("#SasId").val();
+        var Container=$( "#ContainerId option:selected" ).val();
+        var blob="";
+
+        LoadGridFromBlob(CntStr,Container,blob)
+        $("#FormNoLbl").empty();
+        $("#FormNoLbl").append(count+1 + " of " + bloblength );
+    });
+    $('#SearchForm').bind('change', function() {
+        var blob = this.value;
+        var CntStr=$("#SasId").val();
+        var Container=$( "#ContainerId option:selected" ).val();
+        LoadGridFromBlob(CntStr,Container,blob)
+    });
+
+    $( "#nextBtn" ).click(function() {
+
+        var CntStr=$("#SasId").val();
+        var Container=$( "#ContainerId option:selected" ).val();
+        count++;
+        if(count< bloblength)
+        {
+            var blob=bloblist[count];
+            LoadGridFromBlob(CntStr,Container,blob)
+            $("#FormNoLbl").empty();
+            $("#FormNoLbl").append(count+1 + " of " + bloblength );
+        }
+        else
+        {
+            alert("No more files")
+        }
+    });
+
+    $( "#prevbtn" ).click(function() {
+     var CntStr=$("#SasId").val();
+        var Container=$( "#ContainerId option:selected" ).val();
+        count--;
+        if(count >= 0 )
+        {
+            var blob=bloblist[count];
+
+            LoadGridFromBlob(CntStr,Container,blob)
+            $("#FormNoLbl").empty();
+            $("#FormNoLbl").append(count+1 + " of " + bloblength );
+        }
+        else
+        {
+            alert("No more files")
+        }
+  });
+
+});
+
+
+function showGrid(dd)
+{
+            var mydata = [
+               { id: "1", Labels:"Name", question: "Name",answer: JSON.stringify(dd.Name)},
+               { id: "2",Labels:"Company", question: "Company",answer: JSON.stringify(dd.Company)},
+               { id: "3",Labels:"Employee Code",  question: "Employee Code",answer: JSON.stringify(dd['Employee Code'])},
+               { id: "4",Labels:"answer1", question: JSON.stringify(dd.question1).slice(1, -1),answer: JSON.stringify(dd.answer1) },
+               { id: "5",Labels:"answer2",  question: JSON.stringify(dd.question2).slice(1, -1),answer: JSON.stringify(dd.answer2)},
+               { id: "6",Labels:"answer3",  question: JSON.stringify(dd.question3).slice(1, -1),answer: JSON.stringify(dd.answer3)},
+               { id: "7",Labels:"answer4",  question: JSON.stringify(dd.question4).slice(1, -1),answer: JSON.stringify(dd.answer4)},
+               { id: "8", Labels:"answer5", question: JSON.stringify(dd.question5).slice(1, -1),answer: JSON.stringify(dd.answer5)},
+               { id: "9",Labels:"answer6",  question: JSON.stringify(dd.question6).slice(1, -1),answer: JSON.stringify(dd.answer6)},
+               { id: "10", Labels:"answer7", question: JSON.stringify(dd.question7).slice(1, -1),answer: JSON.stringify(dd.answer7)},
+               { id: "11",Labels:"answer8",  question: JSON.stringify(dd.question8).slice(1, -1),answer: JSON.stringify(dd.answer8)},
+               { id: "12",Labels:"answer9",  question: JSON.stringify(dd.question9).slice(1, -1),answer: JSON.stringify(dd.answer9)},
+               { id: "13",Labels:"answer10",  question: JSON.stringify(dd.question10).slice(1, -1),answer: JSON.stringify(dd.answer10)},
+               { id: "14", Labels:"answer11", question: JSON.stringify(dd.question11).slice(1, -1),answer: JSON.stringify(dd.answer11)},
+               { id: "15", Labels:"answer12", question: JSON.stringify(dd.question12).slice(1, -1),answer: JSON.stringify(dd.answer12)},
+               { id: "17", Labels:"answer13", question: JSON.stringify(dd.question13).slice(1, -1),answer: JSON.stringify(dd.answer13)}]
+        $('#jqGrid').jqGrid('clearGridData');
+
+        $("#jqGrid").jqGrid('setGridParam', { data: mydata});
+
+        $("#jqGrid").trigger('reloadGrid');
+
+    $("#jqGrid").jqGrid({
+        datatype: "local",
+        data: mydata,
+        pager: '#jqGridPager',
+        colModel: [
+                    { label: 'No', name: 'id', width: 10, key:true ,hidden:true},
+                    { label: 'Labels', name: 'Labels', width: 10 ,editable: false,hidden:true},
+                    { label: 'Question', name: 'question', width: 600 ,editable: false},
+                    { label: 'Answer', name: 'answer', width: 130,align:'center',editable: true },
+           {
+               label: "Edit",
+
+                name: "actions",
+
+                width: 80,
+              formatter: "actions",
+                search: false,
+                sortable: false,
+                formatoptions: {
+                   keys: true,
+                    editbutton: true,
+                   addOptions: {},
+                   delbutton: false,
+                    delOptions: {
+                     beforeSubmit: function (postdata) { },
+                        onclickSubmit: function (params, Shiftdata) {  },
+                        afterComplete: function (response, Shiftdata) { }
+                         },
+
+                    afterSave: function (rowID, response) {
+                     var item = $(this).jqGrid("getLocalRow", rowID);
+                        UpdateJsonToBlob(item)
+                   },
+               },
+         }
+      ],
+      autowidth:true,
+       shrinkToFit:true,
+
+        rowList: [20, 40, 65, 70],
+         sortorder: "desc",
+        gridview: true,
+        autoencode: true,
+         cellEdit: false,
+        cellsubmit: 'clientArray',
+        editurl: 'clientArray',
+        viewrecords: true,
+        rowNum: 51,
+        emptyrecords: 'No records to display',
+        jsonReader: {
+            root: "rows",
+           page: "page",
+            total: "total",
+           records: "records",
+            repeatitems: true
+ },
+  }).jqGrid("navGrid", "#jqGridPager",
+  {
+    edit: false,
+     add: false,
+     del: false,
+     search: true,
+     refresh: true
+      } );
+}
+
+function UpdateJsonToBlob(data)
+{
+
+     data['count']=count;
+    $.ajax({
+    url: 'UpdateJsonToBlob',
+    type: "POST",
+    data: data,
+    success: function(resp, textStatus, jqXHR) {
+        alert('Updated successfully');
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+        alert('Error occurred!');
+    }
+    });
+}
+
+var bloblist
+var bloblength;
+function LoadGridFromBlob(CntStr,Container,blob)
+{
+        valu={"ConnectionStr":CntStr,"Container":Container,"blob":blob}
+            $.ajax({
+        url: 'LoadGridFromBlob',
+        type: "POST",
+        data:valu,
+        async:false,
+        success: function(resp, textStatus, jqXHR) {
+            bloblist=resp.FileName;
+            bloblength=resp.FileName.length
+             $("#FormList").empty()
+
+            $.each(resp.FileName, function(i, item) {
+              $("#FormList").append($("<option>").attr('value', item));
+            });
+
+            $("#FileName").empty();
+            if(blob=="")
+            {
+               $("#FileName").append(JSON.stringify(resp.FileName[0]).slice(1,-1));
+               $('#formImg').attr('src', "https://checklistform.blob.core.windows.net/checklistfiles/"+ (resp.FileName[0]).slice(7, -5) + ".pdf");
+            }
+            else
+            {
+                 $("#FileName").append(blob);
+                 $('#formImg').attr('src', "https://checklistform.blob.core.windows.net/checklistfiles/"+ blob.slice(7, -5) + ".pdf");
+            }
+            showGrid(resp)
+            $("#prevbtn").prop('disabled', false);
+            $("#nextBtn").prop('disabled', false);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert('No more files!');
+        }
+
+        });
+}
