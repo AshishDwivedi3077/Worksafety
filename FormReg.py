6ccb700843e772520c4aa86694a9888f7b003dbb
AshishDwivedi3077 <68269158+AshishDwivedi3077@users.noreply.github.com>
2020-08-18 12:36:48 +0530

    Add files via upload

--- FormReg.py
+++ FormReg.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template, flash,request,redirect
 import json
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import os, uuid
@@ -6,10 +6,14 @@ import time
 from requests import get, post
 from collections import Counter
 from urllib.request import urlopen
+from werkzeug.utils import secure_filename
+
+
 
 
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 def listtodict(A, dic):
    dic = dict(A)
@@ -251,97 +255,137 @@ def get_key(fields,val):
             return key
     return "key doesn't exist"
 
-@app.route("/AnalyseFIle",methods=['GET','POST'])
+UPLOAD_FOLDER = 'uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/analysefIle",methods=['GET','POST'])
 def AnalyseFIle():
+
+
     resp = request.form
     # blob_service_client = BlobServiceClient.from_connection_string('DefaultEndpointsProtocol=https;AccountName=checklistform;AccountKey=dpZeiTIELCgVGxRxqYkhqAx42b8pWND2onchJ83+yXgHqbkMkPS5aKbmmVMtdDJdENFmmH3EuHhANmUQ02d7TQ==;EndpointSuffix=core.windows.net')
-    blob_service_client = BlobServiceClient.from_connection_string(resp['ConnectionStr'])
-    endpoint = resp['Endpoint']# r"https://checklistformreader.cognitiveservices.azure.com"
-    apim_key =resp['ApimKey']# "e8e591d41a0b43bf8f21da161c204ad2"
-    model_id = resp['ModelID']#'62a96766-80f6-4c23-867b-af07a527d9da'
+    blob_service_client = BlobServiceClient.from_connection_string(resp['SasID'])
+    endpoint = resp['endpointId']# r"https://checklistformreader.cognitiveservices.azure.com"
+    apim_key =resp['ApimkeyId']# "e8e591d41a0b43bf8f21da161c204ad2"
+    model_id = resp['modelId']#'62a96766-80f6-4c23-867b-af07a527d9da'
     post_url = endpoint + "/formrecognizer/v2.0/custom/models/%s/analyze" % model_id
-    filePath = resp['filePath'] #r"C:\Users\ashishkumd2\Desktop\PowerApps\AleSodte_Workspace.pdf"
-    # source = urlopen('https://checklistform.blob.core.windows.net/forms/JhonSeldi_Workspace.pdf')
     blob_client = blob_service_client.get_container_client("forms")
-    FileName=resp['filename']
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    source = 'uploads/'+filename
+    with open(source, "rb") as f:
+        blob_client.upload_blob(filename + '.pdf', f, overwrite=True)
 
-    global source
-    global data_bytes
+
+    # global source
+    # global data_bytes
     result="";
-    try:
 
-        if filePath == '' :
-            print("url")
-            #---------URL-----------------
-            source=urlopen(resp['UrlPath'])
-            data_bytes=source.read()
-            blob_client.upload_blob(resp['filename'] + '.pdf', source, overwrite=True)
+
+        # if filePath == '' :
+        #     print("url")
+        #     #---------URL-----------------
+        #     source=urlopen(resp['UrlPath'])
+        #     data_bytes=source.read()
+        #     blob_client.upload_blob(resp['filename'] + '.pdf', source, overwrite=True)
+        #
+        # else:
+        #     #---------File Path-----------------
+        #     print("file")
+        #     source = resp['filePath']
+        #     with open(source, "rb") as f:
+        #         blob_client.upload_blob(resp['filename'] + '.pdf', f, overwrite=True)
+
+
+    params = {
+        "includeTextDetails": True
+    }
+    headers = {
+        # Request headers
+        'Content-Type': 'application/pdf',
+        'Ocp-Apim-Subscription-Key': apim_key,
+    }
+
+    with open(source, "rb") as f:
+        data_bytes = f.read()
+    try:
+        resp = post(url=post_url, data=data_bytes, headers=headers, params=params)
+        if resp.status_code != 202:
+            print("POST analyze failed:\n%s" % resp.json())
+            quit()
+        print("POST analyze succeeded:")
+        get_url = resp.headers["operation-location"]
+        n_tries = 9
+        n_try = 0
+        wait_sec = 5
+        max_wait_sec = 60
+        while n_try < n_tries:
+            resp_analised = get(url=get_url, headers={"Ocp-Apim-Subscription-Key": apim_key})
+            resp_json = resp_analised.json()
+            if resp_analised.status_code != 200:
+                print("GET analyze results failed:\n%s" % json.dumps(resp_json))
+            status = resp_json["status"]
+            if status == "succeeded":
+                print("Analysis succeeded:")
+                global AnaysedData
+                AnaysedData = json.dumps(resp_json)
+                result = "Analysis succeeded"
+
+
+            if status == "failed":
+                result = "Analysis Failed"
+
+                print("Analysis failed:\n%s" % json.dumps(resp_json))
+            n_try += 1
+
+        #################--upload JSON--########################################
+        blob_client = blob_service_client.get_container_client("analizedforms")
+        blob_client.upload_blob("Result-" + filename[:-4] + ".json", AnaysedData, overwrite=True)
+
+
+        blob_client = blob_service_client.get_container_client("forms")
+        with open(source, "rb") as f:
+            blob_client.upload_blob(filename[:-4] + '.pdf', f, overwrite=True)
+
+        print(AnaysedData)
+    except Exception as e:
+        result="Fail to analyse"
+        print("POST analyze failed:\n%s" % str(e))
+
+    flash(result)
+    os.remove("uploads/" + filename)
+
+    return redirect('/analyseform')
+
+@app.route('/uploadfile', methods=['GET', 'POST'])
+def  upload_file():
+    message=""
+
+
+    if request.method == 'POST':
+        print(request.files)
+        print(request.form["endpointId"])
+        # print(request.modelId)
+
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('no file')
+
+        file = request.files['file']
+        print(file)
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('no filename')
+            message="Fail"
 
         else:
-            #---------File Path-----------------
-            print("file")
-            source = resp['filePath']
-            with open(source, "rb") as f:
-                blob_client.upload_blob(resp['filename'] + '.pdf', f, overwrite=True)
-
-            with open(source, "rb") as f:
-                data_bytes = f.read()
-
-        params = {
-            "includeTextDetails": True
-        }
-        headers = {
-            # Request headers
-            'Content-Type': 'application/pdf',
-            'Ocp-Apim-Subscription-Key': apim_key,
-        }
-
-        # with open(source, "rb") as f:
-        #     data_bytes = f.read()
-        try:
-            resp = post(url=post_url, data=data_bytes, headers=headers, params=params)
-            if resp.status_code != 202:
-                print("POST analyze failed:\n%s" % resp.json())
-                quit()
-            print("POST analyze succeeded:")
-            get_url = resp.headers["operation-location"]
-            n_tries = 9
-            n_try = 0
-            wait_sec = 5
-            max_wait_sec = 60
-            while n_try < n_tries:
-                resp_analised = get(url=get_url, headers={"Ocp-Apim-Subscription-Key": apim_key})
-                resp_json = resp_analised.json()
-                if resp_analised.status_code != 200:
-                    print("GET analyze results failed:\n%s" % json.dumps(resp_json))
-                status = resp_json["status"]
-                if status == "succeeded":
-                    print("Analysis succeeded:")
-                    global AnaysedData
-                    AnaysedData = json.dumps(resp_json)
-                    result = "Analysis succeeded"
-
-                if status == "failed":
-                    result = "Analysis Failed"
-
-                    print("Analysis failed:\n%s" % json.dumps(resp_json))
-                n_try += 1
-
-            #################--upload JSON--########################################
-            blob_client = blob_service_client.get_container_client("analizedforms")
-
-            blob_client.upload_blob("Result-" + FileName + ".json", AnaysedData, overwrite=True)
-
-            print(AnaysedData)
-        except Exception as e:
-            result="Fail to analyse"
-            print("POST analyze failed:\n%s" % str(e))
-
-    except Exception as e:
-        result="Invaid File"
-        print("Invaid File:\n%s" % str(e))
-
-    return result
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print("saved file successfully")
+            message="saved file successfully"
+    return render_template("AnalysedForm.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
